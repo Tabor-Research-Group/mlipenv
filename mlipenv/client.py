320582cecc8ec80fdd577ca2ed6e452d71643a03
b3m2a1 <b3m2a1@gmail.com>
2026-06-07 10:51:56 -0500

    Client affordances for flexible running

--- mlipenv/client.py
+++ mlipenv/client.py
@@ -54,3 +54,47 @@ class MLIPHandler(NodeCommHandler):
                 }
 
         return response
+
+def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+    )
+
+    import sys, os, argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port",
+                        default=os.environ.get(MLIPHandler.DEFAULT_PORT_ENV_VAR, os.environ.get("SESSION_ID")))
+    parser.add_argument("-c", "--no_server", action="store_true", default=False)
+    parser.add_argument("-e", "--env", default='fairchem')
+    parser.add_argument("--allow_scipts",
+                        action="store_true",
+                        default=False)
+    parser.add_argument("request_args",
+                        nargs=argparse.REMAINDER)
+
+    args = parser.parse_args()
+
+    if args.no_server:
+        import subprocess
+        subprocess.run(["conda", "run", "--no-capture-output", "-n", args.env] + args.request_args)
+
+    port = args.port
+    if port is None:
+        raise ValueError(f"`{MLIPHandler.DEFAULT_PORT_ENV_VAR}` must be set at the environment level")
+    port = MLIPHandler.get_valid_port(port)
+
+    MLIP_CONNECTION = ('localhost', MLIPHandler.get_valid_port(port))
+
+    try:  # implicit server startup ping done for every request
+        MLIPHandler.start_server(connection=MLIP_CONNECTION)
+    except OSError:  # server exists
+        if not len(args.request_args):
+            print(f"Already serving on {MLIP_CONNECTION}")
+        pass
+    if len(args.request_args):
+        MLIPHandler.client_request(args.request_args[0], args.request_args[1:], connection=MLIP_CONNECTION)
+
+if __name__ == "__main__":
+    main()
